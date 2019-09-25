64b0874c874b0ae58f4c7aa86d92a5a0e08266cf
ivan006 <ivan.copeland2015@gmail.com>
2019-09-25 16:09:32 +0200

    1

--- resources/js/components/1.php
+++ resources/js/components/1.php
@@ -1,22 +1,36 @@
-
-
 <form id="form" enctype="multipart/form-data" name="" class="" action="http://react.test/store/Group_1" method="post">
-  <input type="hidden" name="_token" value="6Xj6QCZ5YNBl9LpZXAHEZ14YdZu2eS9tfDTb7UFs"> <input class="g-bor-gre" style="display: none;" type="text" name="form" value="data">
+  <input type="hidden" name="_token" value="6Xj6QCZ5YNBl9LpZXAHEZ14YdZu2eS9tfDTb7UFs">
+  <input class="kv-di-no" type="text" name="form" value="data">
+  <br>
+  <h2>Data</h2>
   <ul class="kv-list-parent">
     <li>
       <div class="kv-item-container kv-di-in ">
         <div class="kv-di-in">📁</div>
-        <label style="">
-          <input class="kv-tog-on-ib-switch kv-tog-off-ib-switch" type="checkbox" name="checkbox" value="value"> <input class="kv-field-container kv-name kv-tog-on-ib" type="text" name="Data[content][0][name]" value="_data">
+        <label>
+          <input class="kv-tog-on-ib-switch kv-tog-off-ib-switch" type="checkbox" name="checkbox" value="value">
+          <input class="kv-field-container kv-name kv-tog-on-ib" type="text" name="Data[content]0[name]" value="_data">
           <div class="kv-name-unedit kv-name kv-tog-off-ib ">_data</div>
-          <span class="kv-little-button ">∧</span>
+          <span class="kv-little-button ">^</span>
         </label>
-        <input class="kv-di-no" type="text" name="Data[content][0][type]" value="folder"> <input class="kv-di-no" type="text" name="Data[content][0][id]" value="1"> <input class="kv-di-no" type="text" name="Data[content][0][entity_type]" value="App\Data"> <button type="submit" class="kv-little-button" name="Data[content][0][action]" value="update">✓</button> <button type="submit" class="kv-little-button" name="Data[content][0][action]" value="delete">×</button>
+        <input class="kv-di-no" type="text" name="Data[content]0[type]" value="folder">
+        <input class="kv-di-no" type="text" name="Data[content]0[id]" value="1">
+        <button class="kv-little-button" type="submit" name="Data[content]0[id]">✓</button>
+        <button class="kv-little-button" type="submit" name="Data[content]0[id]">×</button>
         <label class="kv-po-re">
-          <span class="kv-little-button ">+</span> <input class="kv-tog-on-bl-switch" type="checkbox" name="checkbox" value="value">
-          <div class="kv-popover kv-tog-on-bl kv-item-container kv-di-in" style="">
-            <div class=""> <span>📁</span> <input class="kv-field-container kv-name kv-di-in " type="text" name="Data[content][0][add][folder]"> <button type="submit" class="kv-little-button" name="Data[content][0][action]" value="create_folder">+</button> </div>
-            <div class="kv-mar-top-3"> <span>📃</span> <input class="kv-field-container kv-name kv-di-in" type="text" name="Data[content][0][add][file]"> <button type="submit" class="kv-little-button" name="Data[content][0][action]" value="create_file">+</button> </div>
+          <span class="kv-little-button ">+</span>
+          <input class="kv-tog-on-bl-switch" type="checkbox" name="checkbox" value="value">
+          <div class="kv-popover kv-tog-on-bl kv-item-container kv-di-in">
+            <div class="">
+              <span>📁</span>
+              <input class="kv-field-container kv-name kv-di-in " type="text" name="Data[content]0[add]">
+              <button type="submit" class="kv-little-button" name="Data[content]0[action]">+</button>
+            </div>
+            <div class="kv-mar-top-3">
+              <span>📃</span>
+              <input class="kv-field-container kv-name kv-di-in" type="text" name="Data[content]0[add]">
+              <button type="submit" class="kv-little-button" name="Data[content]0[action]">+</button>
+            </div>
           </div>
         </label>
       </div>
@@ -24,33 +38,52 @@
         <li>
           <div class="kv-item-container kv-di-in ">
             <div class="kv-di-in">📃</div>
-            <label style="">
-              <input class="kv-tog-on-ib-switch kv-tog-off-ib-switch" type="checkbox" name="checkbox" value="value"> <input class="kv-field-container kv-name kv-tog-on-ib" type="text" name="Data[content][0][content][0][name]" value="Amenities.txt">
+            <label>
+              <input class="kv-tog-on-ib-switch kv-tog-off-ib-switch" type="checkbox" name="checkbox" value="value">
+              <input class="kv-field-container kv-name kv-tog-on-ib" type="text" name="Data[content]0[content]0[name]" value="Amenities.txt">
               <div class="kv-name-unedit kv-name kv-tog-off-ib ">Amenities.txt</div>
-              <span class="kv-little-button ">∧</span>
+              <span class="kv-little-button ">^</span>
             </label>
-            <input class="kv-di-no" type="text" name="Data[content][0][content][0][type]" value="file"> <input class="kv-di-no" type="text" name="Data[content][0][content][0][id]" value="2"> <button type="submit" class="kv-little-button" name="Data[content][0][content][0][action]" value="update">✓</button> <button type="submit" class="kv-little-button" name="Data[content][0][content][0][action]" value="delete">×</button>
+            <input class="kv-di-no" type="text" name="Data[content]0[content]0[type]" value="file">
+            <input class="kv-di-no" type="text" name="Data[content]0[content]0[id]" value="2">
+            <button class="kv-little-button" type="submit" name="Data[content]0[content]0[id]">✓</button>
+            <button class="kv-little-button" type="submit" name="Data[content]0[content]0[id]">×</button>
           </div>
           <ul class="kv-list-parent">
             <li>
-              <div class="kv-item-container "> <textarea class="kv-field-container kv-content-container kv-di-in" name="Data[content][0][content][0][content]" rows="8">x</textarea> </div>
+              <div class="kv-item-container ">
+                <textarea class="kv-field-container kv-content-container kv-di-in" name="Data[content]0[content]0[content]" rows="8">x</textarea>
+              </div>
             </li>
           </ul>
         </li>
         <li>
           <div class="kv-item-container kv-di-in ">
             <div class="kv-di-in">📁</div>
-            <label style="">
-              <input class="kv-tog-on-ib-switch kv-tog-off-ib-switch" type="checkbox" name="checkbox" value="value"> <input class="kv-field-container kv-name kv-tog-on-ib" type="text" name="Data[content][0][content][1][name]" value="code">
+            <label>
+              <input class="kv-tog-on-ib-switch kv-tog-off-ib-switch" type="checkbox" name="checkbox" value="value">
+              <input class="kv-field-container kv-name kv-tog-on-ib" type="text" name="Data[content]0[content]1[name]" value="code">
               <div class="kv-name-unedit kv-name kv-tog-off-ib ">code</div>
-              <span class="kv-little-button ">∧</span>
+              <span class="kv-little-button ">^</span>
             </label>
-            <input class="kv-di-no" type="text" name="Data[content][0][content][1][type]" value="folder"> <input class="kv-di-no" type="text" name="Data[content][0][content][1][id]" value="26"> <input class="kv-di-no" type="text" name="Data[content][0][content][1][entity_type]" value="App\Data"> <button type="submit" class="kv-little-button" name="Data[content][0][content][1][action]" value="update">✓</button> <button type="submit" class="kv-little-button" name="Data[content][0][content][1][action]" value="delete">×</button>
+            <input class="kv-di-no" type="text" name="Data[content]0[content]1[type]" value="folder">
+            <input class="kv-di-no" type="text" name="Data[content]0[content]1[id]" value="26">
+            <button class="kv-little-button" type="submit" name="Data[content]0[content]1[id]">✓</button>
+            <button class="kv-little-button" type="submit" name="Data[content]0[content]1[id]">×</button>
             <label class="kv-po-re">
-              <span class="kv-little-button ">+</span> <input class="kv-tog-on-bl-switch" type="checkbox" name="checkbox" value="value">
-              <div class="kv-popover kv-tog-on-bl kv-item-container kv-di-in" style="">
-                <div class=""> <span>📁</span> <input class="kv-field-container kv-name kv-di-in " type="text" name="Data[content][0][content][1][add][folder]"> <button type="submit" class="kv-little-button" name="Data[content][0][content][1][action]" value="create_folder">+</button> </div>
-                <div class="kv-mar-top-3"> <span>📃</span> <input class="kv-field-container kv-name kv-di-in" type="text" name="Data[content][0][content][1][add][file]"> <button type="submit" class="kv-little-button" name="Data[content][0][content][1][action]" value="create_file">+</button> </div>
+              <span class="kv-little-button ">+</span>
+              <input class="kv-tog-on-bl-switch" type="checkbox" name="checkbox" value="value">
+              <div class="kv-popover kv-tog-on-bl kv-item-container kv-di-in">
+                <div class="">
+                  <span>📁</span>
+                  <input class="kv-field-container kv-name kv-di-in " type="text" name="Data[content]0[content]1[add]">
+                  <button type="submit" class="kv-little-button" name="Data[content]0[content]1[action]">+</button>
+                </div>
+                <div class="kv-mar-top-3">
+                  <span>📃</span>
+                  <input class="kv-field-container kv-name kv-di-in" type="text" name="Data[content]0[content]1[add]">
+                  <button type="submit" class="kv-little-button" name="Data[content]0[content]1[action]">+</button>
+                </div>
               </div>
             </label>
           </div>
@@ -58,16 +91,22 @@
             <li>
               <div class="kv-item-container kv-di-in ">
                 <div class="kv-di-in">📃</div>
-                <label style="">
-                  <input class="kv-tog-on-ib-switch kv-tog-off-ib-switch" type="checkbox" name="checkbox" value="value"> <input class="kv-field-container kv-name kv-tog-on-ib" type="text" name="Data[content][0][content][1][content][0][name]" value="w3.css">
+                <label>
+                  <input class="kv-tog-on-ib-switch kv-tog-off-ib-switch" type="checkbox" name="checkbox" value="value">
+                  <input class="kv-field-container kv-name kv-tog-on-ib" type="text" name="Data[content]0[content]1[content]0[name]" value="w3.css">
                   <div class="kv-name-unedit kv-name kv-tog-off-ib ">w3.css</div>
-                  <span class="kv-little-button ">∧</span>
+                  <span class="kv-little-button ">^</span>
                 </label>
-                <input class="kv-di-no" type="text" name="Data[content][0][content][1][content][0][type]" value="file"> <input class="kv-di-no" type="text" name="Data[content][0][content][1][content][0][id]" value="27"> <button type="submit" class="kv-little-button" name="Data[content][0][content][1][content][0][action]" value="update">✓</button> <button type="submit" class="kv-little-button" name="Data[content][0][content][1][content][0][action]" value="delete">×</button>
+                <input class="kv-di-no" type="text" name="Data[content]0[content]1[content]0[type]" value="file">
+                <input class="kv-di-no" type="text" name="Data[content]0[content]1[content]0[id]" value="27">
+                <button class="kv-little-button" type="submit" name="Data[content]0[content]1[content]0[id]">✓</button>
+                <button class="kv-little-button" type="submit" name="Data[content]0[content]1[content]0[id]">×</button>
               </div>
               <ul class="kv-list-parent">
                 <li>
-                  <div class="kv-item-container "> <textarea class="kv-field-container kv-content-container kv-di-in" name="Data[content][0][content][1][content][0][content]" rows="8">d</textarea> </div>
+                  <div class="kv-item-container ">
+                    <textarea class="kv-field-container kv-content-container kv-di-in" name="Data[content]0[content]1[content]0[content]" rows="8">d</textarea>
+                  </div>
                 </li>
               </ul>
             </li>
@@ -76,16 +115,22 @@
         <li>
           <div class="kv-item-container kv-di-in ">
             <div class="kv-di-in">📃</div>
-            <label style="">
-              <input class="kv-tog-on-ib-switch kv-tog-off-ib-switch" type="checkbox" name="checkbox" value="value"> <input class="kv-field-container kv-name kv-tog-on-ib" type="text" name="Data[content][0][content][2][name]" value="Rich.txt">
+            <label>
+              <input class="kv-tog-on-ib-switch kv-tog-off-ib-switch" type="checkbox" name="checkbox" value="value">
+              <input class="kv-field-container kv-name kv-tog-on-ib" type="text" name="Data[content]0[content]2[name]" value="Rich.txt">
               <div class="kv-name-unedit kv-name kv-tog-off-ib ">Rich.txt</div>
-              <span class="kv-little-button ">∧</span>
+              <span class="kv-little-button ">^</span>
             </label>
-            <input class="kv-di-no" type="text" name="Data[content][0][content][2][type]" value="file"> <input class="kv-di-no" type="text" name="Data[content][0][content][2][id]" value="28"> <button type="submit" class="kv-little-button" name="Data[content][0][content][2][action]" value="update">✓</button> <button type="submit" class="kv-little-button" name="Data[content][0][content][2][action]" value="delete">×</button>
+            <input class="kv-di-no" type="text" name="Data[content]0[content]2[type]" value="file">
+            <input class="kv-di-no" type="text" name="Data[content]0[content]2[id]" value="28">
+            <button class="kv-little-button" type="submit" name="Data[content]0[content]2[id]">✓</button>
+            <button class="kv-little-button" type="submit" name="Data[content]0[content]2[id]">×</button>
           </div>
           <ul class="kv-list-parent">
             <li>
-              <div class="kv-item-container "> <textarea class="kv-field-container kv-content-container kv-di-in" name="Data[content][0][content][2][content]" rows="8">x</textarea> </div>
+              <div class="kv-item-container ">
+                <textarea class="kv-field-container kv-content-container kv-di-in" name="Data[content]0[content]2[content]" rows="8">x</textarea>
+              </div>
             </li>
           </ul>
         </li>
