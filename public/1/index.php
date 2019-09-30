6dc09d12bf64b32131c9bde9f6569b48abc5817e
ivan006 <ivan.copeland2015@gmail.com>
2019-09-30 17:19:04 +0200

    1

--- public/1/index.php
+++ public/1/index.php
@@ -1,43 +1,78 @@
 <?php
-function solution($number){
-  $maxProduct = $number;
-  $sum = 0;
-  $i = 0;
-  $possibleProduct = 0;
-  while($i <= $maxProduct) {
-
-    // $calc[$i]["product"] = $possibleProduct." (3^".$i."* 5^".$i.")";
-    // $calc[$i]["sum"] = $sum." (3*".$i."+5*".$i.")";
-    if (!function_exists("intergerFactoriseWith3And5")) {
-      // code...
-      function intergerFactoriseWith3And5($possibleProduct){
-        if ($possibleProduct % 3 ==0) {
-          $possibleProduct = $possibleProduct/3;
-          $possibleProduct = intergerFactoriseWith3And5($possibleProduct);
-        } elseif ($possibleProduct % 5 ==0) {
-          $possibleProduct = $possibleProduct/5;
-          $possibleProduct = intergerFactoriseWith3And5($possibleProduct);
-        }
-        return $possibleProduct;
+//Check if a number is prime
+function isPrime($num, $pf = null)
+{
+  if(!is_array($pf))
+  {
+    for($i=2;$i<intval(sqrt($num));$i++) {
+      if($num % $i==0) {
+        return false;
       }
     }
-    // $possibleProduct =
-    // $possibleProduct = intergerFactoriseWith3And5($i);
-    $possibleProduct = $i;
-    $possibleProducts[$i] = $possibleProduct;
-
-
-    $i++;
-
-    // $possibleProduct = pow(3,$i)*pow(5,$i);
-    // $sum = 3*$i+5*$i;
+    return true;
+  } else {
+    $pfCount = count($pf);
+    for($i=0;$i<$pfCount;$i++) {
+      if($num % $pf[$i] == 0) {
+        return false;
+      }
+    }
+    return true;
   }
-
-  // $calc[$i]["sum"] = 5^$i;
-  return $possibleProducts;
 }
 
-$thing = solution(10);
+//Find Prime Factors
+function primeFactors($num)
+{
+  //Record the base
+  $base = intval($num/2);
+  $pf = array();
+  $pn = null;
+  for($i=2;$i <= $base;$i++) {
+    if(isPrime($i, $pn)) {
+      $pn[] = $i;
+      while($num % $i == 0)
+      {
+        $pf[] = $i;
+        $num = $num/$i;
+      }
+    }
+  }
+  return $pf;
+}
+function solution($number){
+  $result = array (
+    2 => array (),
+    3 => array (),
+
+  );
+  for ($i=0; $i < $number; $i++) {
+    $factors = primeFactors($i);
+    $factor_types = array_unique($factors);
+    sort($factor_types);
+
+    // whiteListCombos
+    $wlc1 = array (2);
+    $wlc2 = array (2,3);
+    $wlc3 = array (3);
+
+    if (!empty($factors)) {
+      if ($factor_types == $wlc1 OR $factor_types == $wlc2 OR $factor_types == $wlc3 ) {
+
+        $result[$i]["factors"] = $factors;
+        $sumOfFactors = 0;
+        foreach ($factors as $key => $value) {
+          $sumOfFactors = $sumOfFactors+$value;
+        }
+        $result[$i]["sumOfFactors"] = $sumOfFactors;
+      }
+
+    }
+  }
+  return $result;
+}
+$thing = solution(30);
+$thing = json_encode($thing, JSON_PRETTY_PRINT);
 echo "<pre>";
-var_dump($thing);
+echo $thing;
 ?>
